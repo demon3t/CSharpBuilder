d71007392beed85a3ba5db353883ff507b1d7b1b
Садырин Д.И. <danil.sadirin@mail.ru>
2024-07-13 21:57:56 +0300

    feat(#1): Расширил класс строитель.

--- CSharpBuilder/Builder.cs
+++ CSharpBuilder/Builder.cs
@@ -1,43 +1,110 @@
-﻿namespace CSharpBuilder;
+﻿using CSharpBuilder.Core.Base;
+using System.Text;
 
-/// <summary>
-/// Строитель кода.
-/// </summary>
-public class Builder
+namespace CSharpBuilder
 {
-    #region Конструкторы
-
     /// <summary>
-    /// Коструктор без параметров. 
+    /// Строитель кода.
     /// </summary>
-    public Builder()
+    public class Builder : BaseElementCollection //#doto Расширить BaseElementCollection. Добавить метод добаление элементов. Изменить доступность дочених элементов. 
     {
-        FilePath = string.Empty;
+        #region Конструкторы
+
+        /// <summary>
+        /// Коструктор без параметров. 
+        /// </summary>
+        public Builder()
+        {
+            FilePath = string.Empty;
+            Usings = new List<string>();
+            _hasNamespase = false;
+        }
+
+        /// <summary>
+        /// Коструктор c параметрами.
+        /// </summary>
+        /// <param name="namespace"> Пространство имён. </param>
+        public Builder(string @namespace) : this() //#doto Добавить пространство имён как класс, наследник BaseElement. 
+        {
+            _hasNamespase = true;
+        }
+
+        #endregion Конструкторы
+
+        #region Свойства
+
+        /// <summary>
+        /// Путь записи. 
+        /// </summary>
+        public string FilePath { get; set; }
+
+        /// <summary>
+        /// Директивы.
+        /// </summary>
+        public IList<string> Usings { get; protected set;}
+
+
+        /// <summary>
+        /// Есть пространство имён.
+        /// </summary>
+        private bool _hasNamespase = false;
+
+        #endregion Свойства
+
+        #region Методы : Public
+
+        /// <summary>
+        /// Установить директивы.
+        /// </summary>
+        /// <param name="usings"> Коллекция директив. </param>
+        public void SetUsing(IEnumerable<string> usings)
+        {
+            Usings.Clear();
+
+            foreach (var @using in usings)
+            {
+                Usings.Add(@using.Replace(";", ""));
+            }
+        }
+
+        /// <summary>
+        /// Добавить директивы.
+        /// </summary>
+        /// <param name="usings"> Коллекция директив. </param>
+        public void AddUsing(IEnumerable<string> usings)
+        {
+            foreach (var @using in usings)
+            {
+                var _using = @using.Replace(";", "");
+
+                if (!Usings.Contains(_using))
+                {
+                    Usings.Add(_using);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Записать в файл.
+        /// </summary>
+        /// <param name="filePath"> Полный путь к файлу. </param>
+        public void WriteToFile(string filePath)
+        {
+            if (string.IsNullOrEmpty(filePath))
+            {
+                throw new ArgumentNullException(nameof(filePath));
+            }
+
+            var sb = new StringBuilder();
+
+            foreach (var line in Content)
+            {
+                sb.AppendLine(line);
+            }
+
+            File.WriteAllText(filePath, sb.ToString());
+        }
+
+        #endregion Методы : Public
     }
-
-    /// <summary>
-    /// Коструктор c параметрами.
-    /// </summary>
-    /// <param name="filePath"> Путь записи. </param>
-    public Builder(string filePath)
-    {
-        FilePath = filePath;
-    }
-
-    #endregion Конструкторы
-
-    #region Свойства
-
-    /// <summary>
-    /// Путь записи. 
-    /// </summary>
-    public string FilePath { get; set; }
-
-    #endregion Свойства
-
-    #region Методы : Public
-
-    
-
-    #endregion Методы : Public
 }
