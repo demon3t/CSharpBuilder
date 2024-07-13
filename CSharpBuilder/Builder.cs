9ee1184db726bdaff00d9fdecf7fffb52b4b812d
Садырин Д.И. <danil.sadirin@mail.ru>
2024-07-13 18:25:21 +0300

    first commit

---
+++ CSharpBuilder/Builder.cs
@@ -0,0 +1,43 @@
+﻿namespace CSharpBuilder;
+
+/// <summary>
+/// Строитель кода.
+/// </summary>
+public class Builder
+{
+    #region Конструкторы
+
+    /// <summary>
+    /// Коструктор без параметров. 
+    /// </summary>
+    public Builder()
+    {
+        FilePath = string.Empty;
+    }
+
+    /// <summary>
+    /// Коструктор c параметрами.
+    /// </summary>
+    /// <param name="filePath"> Путь записи. </param>
+    public Builder(string filePath)
+    {
+        FilePath = filePath;
+    }
+
+    #endregion Конструкторы
+
+    #region Свойства
+
+    /// <summary>
+    /// Путь записи. 
+    /// </summary>
+    public string FilePath { get; set; }
+
+    #endregion Свойства
+
+    #region Методы : Public
+
+    
+
+    #endregion Методы : Public
+}
